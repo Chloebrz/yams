290e4d4fd8d5f63354da3d7020b1f6d9589cc162
Chloebrz <chloe.brouzes@hotmail.fr>
2014-05-10 14:47:23 +0200

    Création du projet Yams avec libGX

---
+++ desktop/src/fr/mbpmx/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package fr.mbpmx.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import fr.mbpmx.game.YamsMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new YamsMain(), config);
+	}
+}
---
+++ html/src/fr/mbpmx/game/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package fr.mbpmx.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import fr.mbpmx.game.YamsMain;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new YamsMain();
+        }
+}

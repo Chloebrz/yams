455de8707afb1439d4ebd503af1364cf5e531993
Maxime <maxlasserre@free.fr>
2014-05-30 17:49:44 +0200

    Change ScoreTable construction

--- core/src/fr/mbpmx/model/ScoreTable.java
+++ core/src/fr/mbpmx/model/ScoreTable.java
@@ -2,6 +2,11 @@ package fr.mbpmx.model;
 
 import java.util.Hashtable;
 
-public class ScoreTable extends Hashtable<K, V> {
+public class ScoreTable extends Hashtable<Combination, Integer> {
 
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    
 }

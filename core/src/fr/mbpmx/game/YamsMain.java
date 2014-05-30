d0c36c7e15442eecc27906afe55ca9f732e941c0
Chloebrz <chloe.brouzes@hotmail.fr>
2014-05-30 18:55:54 +0200

    Creation of a presentation screen, which will appear and disappear automatically at the launch of the application. Do we use TweenManager?

--- core/src/fr/mbpmx/game/YamsMain.java
+++ core/src/fr/mbpmx/game/YamsMain.java
@@ -1,27 +1,24 @@
 package fr.mbpmx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import fr.mbpmx.other.YamsPresentation;
 
 public class YamsMain extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+	public final static String TITLE = "Yam's", VERSION = "1";
+
 	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+	public void create() {
+		((Game) Gdx.app.getApplicationListener())
+				.setScreen(new YamsPresentation());
 	}
 
 	@Override
-	public void render () {
+	public void render() {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
 	}
 }
--- core/src/fr/mbpmx/model/Combination.java
+++ core/src/fr/mbpmx/model/Combination.java
@@ -1,31 +1,26 @@
 package fr.mbpmx.model;
 
 public enum Combination {
-    ONE,
-    TWO,
-    THREE,
-    FOUR,
-    FIVE,
-    SIX,
-    TWOPAIRS(15, 25),
-    THREEOFAKIND(20, 30),
-    FULLHOUSE(25, 35),
-    FOUROFAKIND(30, 40),
-    STRAIGHT(40, 40),
-    YAMS(50, 60),
-    PLUS,
-    MINUS,
-    CHANCE;
-    
-    private int normalValue;
-    private int bonusValue;
-    
-    Combination(){
-        
-    }
-    
-    Combination(int normal, int withBonus){
-        this.normalValue = normal;
-        this.bonusValue = withBonus;
-    }
+	ONE, TWO, THREE, FOUR, FIVE, SIX, TWOPAIRS(15, 25), THREEOFAKIND(20, 30), FULLHOUSE(
+			25, 35), FOUROFAKIND(30, 40), STRAIGHT(40, 40), YAMS(50, 60), PLUS, MINUS, CHANCE;
+
+	private int normalValue;
+	private int bonusValue;
+
+	Combination() {
+
+	}
+
+	Combination(int normal, int withBonus) {
+		this.normalValue = normal;
+		this.bonusValue = withBonus;
+	}
+
+	public int getNormalValue() {
+		return normalValue;
+	}
+
+	public int getBonusValue() {
+		return bonusValue;
+	}
 }

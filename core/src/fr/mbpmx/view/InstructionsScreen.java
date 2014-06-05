499e1e9fff62739ab094950e1d679f6eaaee9945
Chloebrz <chloe.brouzes@hotmail.fr>
2014-06-05 21:01:13 +0200

    Small text on the Instructions screen

--- core/src/fr/mbpmx/view/InstructionsScreen.java
+++ core/src/fr/mbpmx/view/InstructionsScreen.java
@@ -2,6 +2,8 @@ package fr.mbpmx.view;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
@@ -10,60 +12,76 @@ import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import fr.mbpmx.game.YamsMain;
 
 public class InstructionsScreen extends YamsScreen {
-    private TextButton buttonBack;
+	private TextButton buttonBack;
+	private String instructionsText;
 
-    @Override
-    public void render(float delta) {
-        super.render(delta);
-    }
+	private SpriteBatch batch;
+	private BitmapFont font;
 
-    @Override
-    public void resize(int width, int height) {
+	@Override
+	public void render(float delta) {
+		super.render(delta);
 
-    }
+		batch.begin();
+		font.draw(batch, instructionsText, 200, 200);
+		batch.end();
+	}
 
-    @Override
-    public void show() {
-        super.show();
-        
-        //Creating heading
-        Label heading = new Label(YamsMain.TITLE, skin, "big");
-        heading.setFontScale(2);
+	@Override
+	public void resize(int width, int height) {
 
-        // Creation of the buttons
-        buttonBack = new TextButton("Back", skin);
-        buttonBack.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                ((Game) Gdx.app.getApplicationListener())
-                        .setScreen(new MainMenuScreen());
-            }
-        });
-        buttonBack.pad(15); // TODO Select the right padding and place the
-                            // button in a table
-        table.add(heading).spaceBottom(75).row();
-        table.add(buttonBack).spaceBottom(15).row();
-        
-        stage.addActor(table);
-    }
+	}
 
-    @Override
-    public void hide() {
+	@Override
+	public void show() {
+		super.show();
 
-    }
+		// Creating heading
+		Label heading = new Label(YamsMain.TITLE, skin, "big");
+		heading.setFontScale(2);
 
-    @Override
-    public void pause() {
+		// Creating the instructions text
+		instructionsText = new String(
+				"Here will be the instructions of our Yam's game.");
+		batch = new SpriteBatch();
+		font = new BitmapFont();
 
-    }
+		// Creation of the button
+		buttonBack = new TextButton("Back", skin, "small");
+		buttonBack.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				((Game) Gdx.app.getApplicationListener())
+						.setScreen(new MainMenuScreen());
+			}
+		});
 
-    @Override
-    public void resume() {
+		table.add(heading).spaceBottom(75).row();
+		// table.add(instructionsText).spaceBottom(20).row();
+		table.add(buttonBack).right().spaceBottom(15);
 
-    }
+		stage.addActor(table);
+	}
 
-    @Override
-    public void dispose() {
-        super.dispose();
-    }
+	@Override
+	public void hide() {
+
+	}
+
+	@Override
+	public void pause() {
+
+	}
+
+	@Override
+	public void resume() {
+
+	}
+
+	@Override
+	public void dispose() {
+		super.dispose();
+		batch.dispose();
+		font.dispose();
+	}
 }
